e7dfb91d13411d0125e6d0bd4f73be4fefdb69f2
zqua <86474462+zquaa@users.noreply.github.com>
2021-06-25 17:11:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-#I hardly recommend using login system, because hwid is not as safe as it and can get cracked easily.
+#Make sure that this code is above your current one.
 
+#Imports
 import subprocess, requests, time, os
 
+#Strings
 hardwareid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-#Type any site that contains easily accesable text.
 site = requests.get('TYPE SITE HERE')
-#The only examples i can think of are raw pastebin and raw github text
 
 
+#Actual Authentication System
 try:
     if hardwareid in site.text:
         pass
     else:
-        print('[ERROR] HWID Not in database')
+        print('[ERROR] HWID NOT in database')
         print('[HWID:' + hardwareid + ']') 
         time.sleep(5)
         os._exit()
 except:
-    print('[ERROR] Failed to connect to database')
+    print('[ERROR] FAILED to connect to database')
     time.sleep(5) 
     os._exit() 
     
-
+#Your actual project comes under here:
 print('Success.')
 input()
